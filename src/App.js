2ec6a2ff5c4a3c68cd8dc106bcb15de7153a4451
Matan <email>
2021-10-05 23:32:11 +0300

    Installed axios package to send requests with help of the created functions: getTasks, postTask and updateTask, added use Effect for sorting the tasks from db to the states in the first render (checking their checked field)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import AddTask from './AddTask'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import uuid from 'react-uuid'
 import TasksContainer from './TasksContainer'
+import axios from 'axios'
 
 const App = () => {
+  var serverIp = "http://localhost:80"
+
   const [tasksToDo, setTasksToDo] = useState([
 
   ])
@@ -12,10 +15,49 @@ const App = () => {
 
   ])
 
+  const getTasks = async () => {
+    return await axios.get(serverIp +"/api/tasks").then(res => res.data)
+    
+  }
+  
+  const postTask = (newTask) => {
+    console.log("Task to add", newTask)
+    axios.post(serverIp + "/api/task", newTask)
+  }
+
+  const updateTask = (taskId, value) => {
+    console.log("TasksId to change", taskId)
+    axios.put(serverIp + "/api/tasks/"+ taskId, {
+      "Checked": value
+    })
+  }
+
+  useEffect(() => {
+    async function sortTasks() {
+      var allTasks = await getTasks()
+      var tCompleted = []
+      var tToDo = []
+
+      allTasks.map((task) => {
+        if (task.Checked) {
+          tCompleted.push(task)
+        }
+        else {
+          tToDo.push(task)
+        }
+      })
+
+      setCompletedTasks(tCompleted)
+      setTasksToDo(tToDo)
+    }
+    sortTasks()
+  }, [])
+
   const onAdd = (text, priority) => {
     var taskId = uuid();
-    var newTask = {Text: text, Priority: priority, Id: taskId}
+    var newTask = {Text: text, Priority: priority, Checked: false, id: taskId}
     setTasksToDo([...tasksToDo, newTask])
+    postTask(newTask)
     console.log("Tasks To Do - ", tasksToDo)
    }
 
@@ -25,12 +67,14 @@ const App = () => {
       setCompletedTasks([...completedTasks, task])
       setTasksToDo(tasksToDo.filter(t => t !== task))
       console.log("Tasks To Do - ", tasksToDo)
+      updateTask(task.id, isChecked)
     }
     else
     {
       setTasksToDo([...tasksToDo, task])
       setCompletedTasks(completedTasks.filter(t => t !== task))
       console.log("Completed Tasks - ", completedTasks)
+      updateTask(task.id, isChecked)
     }
   }
 
